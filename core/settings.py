18df8a547a5d2dbedcf398eff18d54f3b5089e32
sherzod <sherzod@kali>
2023-10-14 17:27:31 +0500

    first commigt

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n1iig)0*ci1$c1#6f87ie)gtglie%36u2=s@8z_x9v-gq92(0b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['91.92.136.224']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
